995118f99bdbb669dc6aa6abf1e691112c0ae4ca
isakya <1016361268@qq.com>
2023-05-05 12:27:20 +0800

    项目初始化

---
+++ src/main/java/com/izumi/IzumiBootApplication.java
@@ -0,0 +1,12 @@
+package com.izumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzumiBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IzumiBootApplication.class, args);
+    }
+}
---
+++ src/test/java/com/izumi/IzumiBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzumiBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
