5168b33cd1e7194c113e9029dccf3e11fce1a498
isakya <1016361268@qq.com>
2023-05-05 14:36:55 +0800

    SpringBoot连接数据库

--- src/main/java/com/izumi/controller/UserController.java
+++ src/main/java/com/izumi/controller/UserController.java
@@ -1,14 +1,24 @@
 package com.izumi.controller;
 
+import cn.hutool.db.DbUtil;
+import cn.hutool.db.Entity;
 import com.izumi.exception.ServiceException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.List;
+
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class UserController {
+    private final DataSource dataSource;
+
     @GetMapping("/hello")
     public String hello() {
         log.debug("UserController.hello");
@@ -16,10 +26,12 @@ public class UserController {
     }
 
     @PostMapping("/user/save")
-    public String save(String userName, String password) {
+    public List<Entity> save(String userName, String password) throws SQLException {
         if (userName == null || password == null) {
             ServiceException.throwBiz(999, "用户名或密码不能为空");
         }
-        return userName + "/" + password;
+        System.out.println(dataSource);
+        List<Entity> list = DbUtil.use().findAll("sys_user");
+        return list;
     }
 }
