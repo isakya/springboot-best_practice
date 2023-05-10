d054392a0c9f10fc3a410c0f692f193902f9db65
isakya <1016361268@qq.com>
2023-05-10 11:34:57 +0800

    @Value、Envirnoment两种方式读取配置文件

--- src/main/java/com/izumi/auth/impl/RedisTokenStore.java
+++ src/main/java/com/izumi/auth/impl/RedisTokenStore.java
@@ -1,10 +1,13 @@
 package com.izumi.auth.impl;
 
 
+import cn.hutool.core.convert.Convert;
 import com.izumi.auth.ITokenStore;
 import com.izumi.modules.sys.vo.LoginVO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +20,27 @@ import java.util.concurrent.TimeUnit;
 public class RedisTokenStore implements ITokenStore {
     private final RedisTemplate<String, LoginVO> redisTemplate;
     private final String PREFIX = "TOKEN_STORE:";
+
+    /**
+     * 读取配置文件的值
+     */
+    // 方式一：@Value --必须配默认值，不然报错
+    @Value("${auth.expire:2}")
+    public long expire;
+
+    // 方式二：
+    private final Environment environment;
+    
     @Override
     public void setToken(LoginVO vo) {
         String key = PREFIX + vo.getToken();
         redisTemplate.opsForValue().set(key, vo);
         // 设置有效期
-        redisTemplate.expire(key, 2, TimeUnit.HOURS);
+        redisTemplate.expire(key, expire, TimeUnit.HOURS);
+        String expire = environment.getProperty("auth.expire");
+        // 转换类型，如果转不成功则默认为2L
+        Long lexpire = Convert.toLong(expire, 2L);
+        System.err.println(lexpire);
     }
 
     @Override
