9e38d19cc438e7d43d1b3838f6efe376c7968cc0
isakya <1016361268@qq.com>
2023-05-10 16:39:51 +0800

    构建请求日志实体

--- src/main/java/com/izumi/interceptor/LogInterceptor.java
+++ src/main/java/com/izumi/interceptor/LogInterceptor.java
@@ -1,11 +1,11 @@
 package com.izumi.interceptor;
 
 import com.izumi.holder.LoginUserHolder;
-import com.izumi.modules.sys.vo.LoginVO;
+import com.izumi.log.LogHolder;
+import com.izumi.log.LogParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,17 +18,22 @@ public class LogInterceptor implements HandlerInterceptor {
         String uri = request.getRequestURI();
         Long userId = LoginUserHolder.getUserId();
         String userName = LoginUserHolder.getUserName();
-        log.debug("LogInterceptor-preHandle,uri:{},userId:{},userName:{}",uri,userId,userName);
+        LogParam logParam = new LogParam();
+        logParam.setUserId(userId);
+        logParam.setUserName(userName);
+        logParam.setUrl(uri);
+        LogHolder.set(logParam);
         return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        log.debug("LogInterceptor-postHandle");
-    }
-
-    @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        log.debug("LogInterceptor-afterCompletion");
+        try {
+            LogHolder.writeLog();
+        } catch(Exception e) {
+            log.error("异常", e);
+        } finally {
+            LogHolder.remove();
+        }
     }
 }
---
+++ src/main/java/com/izumi/log/LogHolder.java
@@ -0,0 +1,37 @@
+package com.izumi.log;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogHolder {
+    public static ThreadLocal<LogParam> LOG_THEAD_LOCAL = new ThreadLocal<>();
+
+    /**
+     * 设置当前请求日志信息
+     * @param vo
+     */
+    public static void set(LogParam vo) {
+        LOG_THEAD_LOCAL.set(vo);
+    }
+
+    /**
+     * 获取当前请求日志信息
+     * @return
+     */
+    public static LogParam get() {
+        return LOG_THEAD_LOCAL.get();
+    }
+
+    /**
+     * 删除当前请求日志信息
+     */
+    public static void remove() {
+        LOG_THEAD_LOCAL.remove();
+    }
+
+    public static void writeLog() {
+        LogParam param = LOG_THEAD_LOCAL.get();
+        if(param == null) return;
+        log.debug("userId:{},userName:{},url:{}",param.getUserId(),param.getUserName(),param.getUrl());
+    }
+}
